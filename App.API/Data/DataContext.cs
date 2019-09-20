857880fe6c49ead8be5a8d9a2289450100c113eb
shayar <shayarshrestha23@gmail.com>
2019-09-20 22:37:35 +0545

    Initial Commit

---
+++ App.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using App.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
