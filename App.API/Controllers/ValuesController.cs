c257701a51e92f3cc1016da4b2b412620ec6208f
shayar <shayarshrestha23@gmail.com>
2019-10-28 13:27:51 +0545

    Client Side login and Register

--- App.API/Controllers/ValuesController.cs
+++ App.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace App.API.Controllers
             _context = context;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
