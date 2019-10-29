1b220bfda87f2a192a15485c484eb191f93aec3d
shayar <shayarshrestha23@gmail.com>
2019-10-29 09:02:59 +0545

    Changes

--- App-SPA/src/app/nav/nav.component.ts
+++ App-SPA/src/app/nav/nav.component.ts
@@ -40,6 +40,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
-    this.router.navigate(['/home']);
+    this.router.navigate(['/']);
   }
 }
